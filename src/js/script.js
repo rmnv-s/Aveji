2bc12b9fe28d15dcc88d47973ffebcc537c5d2ea
dev <remnev.dev@gmail.com>
2023-08-08 18:31:56 +0300

    style(mobile): full page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-// Получаем все элементы materials-list__item
 const items = document.querySelectorAll('.materials-list__item');
 
 // Перебираем каждый элемент
@@ -13,8 +12,39 @@ items.forEach((item) => {
     // Получаем картинку внутри текущего элемента
     const image = item.querySelector('.materials-list__item-img');
 
-    // Устанавливаем позицию картинки относительно курсора мыши
-    image.style.left = `${x}px`;
-    image.style.top = `${y}px`;
+    // Устанавливаем сдвиг картинки относительно курсора мыши
+    const imageOffsetX = -250;
+    const imageOffsetY = 0;
+
+    // Устанавливаем позицию картинки с учетом сдвига и ограничиваем координаты
+    const maxX = rect.width - image.offsetWidth;
+    const maxY = rect.height - image.offsetHeight;
+    const finalX = Math.max(0, Math.min(x + imageOffsetX, maxX));
+    const finalY = Math.max(0, Math.min(y - imageOffsetY, maxY));
+
+    // Устанавливаем позицию картинки с помощью transform
+    image.style.transform = `translate(${finalX}px, ${finalY}px)`;
+  });
+});
+
+// BURGER MENU
+const root = document.querySelector('.root');
+const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.menu__list-link');
+const menuListSocial = document.querySelector('.menu__list-social');
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger-menu__open');
+  menu.classList.toggle('active-menu');
+  root.classList.toggle('overflow');
+  menuListSocial.classList.toggle('menu__list-social-active');
+});
+
+menuLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger-menu__open');
+    menu.classList.remove('active-menu');
+    root.classList.remove('overflow');
   });
 });
